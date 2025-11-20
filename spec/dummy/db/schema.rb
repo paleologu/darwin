437a9feb321de469c17b070e2a41d3c41e51ee94
Mihail Paleologu <mihailpaleologu@gmail.com>
2025-11-21 00:06:20 +0200

    debug schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -14,11 +14,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_09_26_150201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "darwin_articles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "darwin_blocks", force: :cascade do |t|
     t.jsonb "args", default: {}
     t.string "block_type", null: false
@@ -29,11 +24,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_09_26_150201) do
     t.integer "position", default: 0
     t.datetime "updated_at", null: false
     t.index ["model_id"], name: "index_darwin_blocks_on_model_id"
-  end
-
-  create_table "darwin_journalists", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "darwin_models", force: :cascade do |t|
