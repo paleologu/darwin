011667279b4afe9235d787813681885ca86dbffe
Mihail Paleologu <mihailpaleologu@gmail.com>
2025-12-03 14:47:32 +0200

    use solidqueue in the dummy app

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -53,8 +53,9 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
-  # Run background work on the built-in async adapter in development.
-  config.active_job.queue_adapter = :async
+  # Run background work on Solid Queue in development.
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Highlight code that triggered redirect in logs.
   config.action_dispatch.verbose_redirect_logs = true
